183f81c50686a207c2bc1f231d4bfebf0bf876f6
Spencer, Paul Whiting <pwablito@gmail.com>
2021-08-27 11:03:49 -0600

    Fix read bytes problem

--- command/gpu/nvidia.py
+++ command/gpu/nvidia.py
@@ -8,4 +8,4 @@ class NvidiaSMICommand(command.Command):
         self.command = 'nvidia-smi --query-gpu=timestamp,name,pci.bus_id,driver_version,pstate,pcie.link.gen.max,pcie.link.gen.current,temperature.gpu,utilization.gpu,utilization.memory,memory.total,memory.free,memory.used --format=csv'  # noqa E501
 
     def get_output_data(self):
-        return list(csv.DictReader(io.StringIO(self.stdout)))
+        return list(csv.DictReader(io.StringIO(self.stdout.decode())))
