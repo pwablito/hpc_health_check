bcc19760dc113a6b2162825d9d62fea381d3dd17
Spencer, Paul Whiting <pwablito@gmail.com>
2021-09-08 17:03:04 -0600

    flake8 compliance

--- tests/test.py
+++ tests/test.py
@@ -4,4 +4,6 @@ import tests.config
 
 def run_tests():
     # TODO Figure out how to combine test suites
-    unittest.TextTestRunner().run(unittest.TestLoader().loadTestsFromModule(tests.config))
+    unittest.TextTestRunner().run(
+        unittest.TestLoader().loadTestsFromModule(tests.config)
+    )
